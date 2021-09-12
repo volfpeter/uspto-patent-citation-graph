9ccfa9848d4f1de77f3563f0e804c354ec4af97c
Peter Volf <do.volfp@gmail.com>
2021-09-12 21:21:18 +0200

    bumped version + formatting

--- uspto_patent_citation_graph/__init__.py
+++ uspto_patent_citation_graph/__init__.py
@@ -12,14 +12,18 @@ __copyright__ = "Copyright 2020, Peter Volf"
 __email__ = "do.volfp@gmail.com"
 __license__ = "MIT"
 __url__ = "https://github.com/volfpeter/uspto-patent-citation-graph"
-__version__ = "0.2002.0"
+__version__ = "0.2109.0"
 
 PATENTS_VIEW_ENDPOINT = "https://api.patentsview.org/patents/query"
 
 
 class USPTOPatentCitationGraph(Graph):
-
-    def __init__(self, database: Optional[GraphDatabaseInterface], *, log_neighbor_loading: bool = False) -> None:
+    def __init__(
+        self,
+        database: Optional[GraphDatabaseInterface],
+        *,
+        log_neighbor_loading: bool = False,
+    ) -> None:
         """
         Initialization.
 
@@ -38,9 +42,11 @@ class USPTOPatentCitationGraph(Graph):
             self._logger.setLevel(logging.DEBUG)
             handler: logging.Handler = logging.StreamHandler()
             handler.setLevel(logging.DEBUG)
-            handler.setFormatter(logging.Formatter(
-                "%(levelname)s | %(asctime)s | %(name)s\n -- %(message)s"
-            ))
+            handler.setFormatter(
+                logging.Formatter(
+                    "%(levelname)s | %(asctime)s | %(name)s\n -- %(message)s"
+                )
+            )
             self._logger.addHandler(handler)
 
     @staticmethod
@@ -57,7 +63,9 @@ class USPTOPatentCitationGraph(Graph):
         from sqlalchemy.pool import StaticPool
 
         Base = declarative_base()
-        engine = sqlalchemy.create_engine("sqlite:///USPTOPatentCitationGraph.db", poolclass=StaticPool)
+        engine = sqlalchemy.create_engine(
+            "sqlite:///USPTOPatentCitationGraph.db", poolclass=StaticPool
+        )
         Session = sessionmaker(bind=engine)
 
         dbi: GraphDatabaseInterface = create_graph_database_interface(
@@ -93,7 +101,12 @@ class USPTOPatentCitationNode(Node):
         graph.log_neighbor_loading(self.name)
 
         query = {"patent_number": self.name}
-        fields = ["cited_patent_number", "cited_patent_title", "citedby_patent_number", "citedby_patent_title"]
+        fields = [
+            "cited_patent_number",
+            "cited_patent_title",
+            "citedby_patent_number",
+            "citedby_patent_title",
+        ]
         response = requests.post(PATENTS_VIEW_ENDPOINT, json={"q": query, "f": fields})
         if response.status_code != 200:
             raise ValueError("Request failed")
@@ -102,10 +115,11 @@ class USPTOPatentCitationNode(Node):
         cited_patents = patents.get("cited_patents", [])
         citedby_patents = patents.get("citedby_patents", [])
 
-
         nodes = graph.nodes
 
-        for patent, prefix in chain(zip(cited_patents, repeat("cited")), zip(citedby_patents, repeat("citedby"))):
+        for patent, prefix in chain(
+            zip(cited_patents, repeat("cited")), zip(citedby_patents, repeat("citedby"))
+        ):
             patent_number = patent.get(f"{prefix}_patent_number")
             patent_title = patent.get(f"{prefix}_patent_title")
             if patent_number is None or patent_title is None:
@@ -114,7 +128,7 @@ class USPTOPatentCitationNode(Node):
             neighbor = nodes.get_node_by_name(
                 patent_number.strip(),
                 can_validate_and_load=True,
-                external_id=patent_title.strip()
+                external_id=patent_title.strip(),
             )
 
             if neighbor is not None:
@@ -122,6 +136,9 @@ class USPTOPatentCitationNode(Node):
 
 
 class USPTOPatentCitationNodeList(NodeList):
-
-    def _create_node(self, index: int, name: str, external_id: Optional[str] = None) -> Node:
-        return USPTOPatentCitationNode(graph=self._graph, index=index, name=name, external_id=external_id)
+    def _create_node(
+        self, index: int, name: str, external_id: Optional[str] = None
+    ) -> Node:
+        return USPTOPatentCitationNode(
+            graph=self._graph, index=index, name=name, external_id=external_id
+        )
